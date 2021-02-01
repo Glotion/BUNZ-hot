b74906fa146ec93dca357353dcda4f012f89718f
WwwZMing <44491130+WwwZMing@users.noreply.github.com>
2021-02-01 18:35:38 +0800

    main
    用协程重新写了一遍

---
+++ newdemo/Assets/Scripts/Base/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.Internet;
+using UnityEditor;
+using UnityEngine;
+/// <summary>
+///     author:WenZeming
+///     启动函数
+/// </summary>
+namespace Assets.Scripts.Base
+{
+    [InitializeOnLoad]
+    class Startup
+    {
+        //进入启动事件
+        static Startup()
+        {
+            if (!CheckNetworkActiveService.CheckNetworkActive())
+            {
+                Debug.Log("游戏无网络，请检查连接或退出");
+            }
+            Debug.Log("Up and running");
+            
+        }
+    }
+}
---
+++ newdemo/Assets/Scripts/DownloadMoudle/RequestBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Assets.Scripts.DownloadMoudle
+{
+    public class RequestBase: IDisposable
+    {
+        UnityWebRequest webRequest;
+        string url;
+        string fileName;
+
+        public UnityWebRequest WebRequest { get => webRequest; set => webRequest = value; }
+        public string Url { get => url; set => url = value; }
+        public string FileName { get => fileName; set => fileName = value; }
+
+        public RequestBase(string _url, string _fileName)
+        {
+            Url = _url;
+            WebRequest = UnityWebRequest.Get(Url);
+            FileName = _fileName;
+        }
+        //发送头请求
+        public RequestBase(string _url, string _fileName, bool isHead)
+        {
+            Url = _url;
+            WebRequest = UnityWebRequest.Head(Url);
+            FileName = _fileName;
+        }
+
+        public void Dispose()
+        {
+            WebRequest.Dispose();
+        }
+    }
+}
